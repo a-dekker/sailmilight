a1f6025c67689c7e0fea59516fc1fa194173c2d7
Quent-in <pages_quentin@hotmail.com>
2017-02-13 21:30:52 +0100

    FR locale

---
+++ translations/harbour-sailmilight-fr.ts
@@ -0,0 +1,187 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="24"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="44"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="49"/>
+        <source>WiFi controller for various rgbw light bulbs</source>
+        <translation>Contrôleur WiFi pour de nombreuses marques d'ampoule rgbw</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="58"/>
+        <source>Author</source>
+        <translation>Auteur</translation>
+    </message>
+</context>
+<context>
+    <name>EffectsPage</name>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="46"/>
+        <source>Light effects</source>
+        <translation>Effets lumineux</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="93"/>
+        <source>Rainbow</source>
+        <translation>Arc-en-ciel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="99"/>
+        <source>Disco</source>
+        <translation>Disco</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="105"/>
+        <source>Pulse</source>
+        <translation>Impulsion</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="154"/>
+        <source>Flash red</source>
+        <translation>Clignotant rouge</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="160"/>
+        <source>Flash green</source>
+        <translation>Clignotant vert</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="166"/>
+        <source>Flash blue</source>
+        <translation>Clignotant bleu</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="218"/>
+        <source>Fade rgbw</source>
+        <translation>Fondu rgbw</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="212"/>
+        <source>Fade white</source>
+        <translation>Fondu blanc</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EffectsPage.qml" line="228"/>
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="77"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="81"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="85"/>
+        <source>Active zone: </source>
+        <translation>Zone activée :</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="86"/>
+        <source>all</source>
+        <translation>toutes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="112"/>
+        <location filename="../qml/pages/MainPage.qml" line="120"/>
+        <source>ON</source>
+        <translation>MARCHE</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="114"/>
+        <source>Switch the RGBW bulb ON/OFF</source>
+        <translation>Basculer l'ampoule sur MARCHE/ARRÊT</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="127"/>
+        <source>OFF</source>
+        <translation>ARRÊT</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="142"/>
+        <source>Brightness</source>
+        <translation>Luminosité</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="158"/>
+        <source>Red</source>
+        <translation>Rouge</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="174"/>
+        <source>Green</source>
+        <translation>Vert</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="190"/>
+        <source>Blue</source>
+        <translation>Bleu</translation>
+    </message>
+</context>
+<context>
+    <name>SettingPage</name>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="70"/>
+        <source>Invalid IP address</source>
+        <translation>Adresse IP invalide</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="104"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="107"/>
+        <source>Wifi controller</source>
+        <translation>Controleur Wifi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="123"/>
+        <source>IP address</source>
+        <translation>Adresse IP</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="284"/>
+        <source>Port number</source>
+        <translation>Numéro de port</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="330"/>
+        <source>Search WiFi bridges</source>
+        <translation>Rechercher les ponts WiFi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="333"/>
+        <source>Discovered IP: </source>
+        <translation>IP découverte : </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingPage.qml" line="334"/>
+        <source>Corresponding MAC: </source>
+        <translation>MAC correspondante : </translation>
+    </message>
+</context>
+</TS>
